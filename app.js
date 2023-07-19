567819affd31b252aa85aa84c393575b7972e980
jstillword <130879326+jstillword@users.noreply.github.com>
2023-07-19 11:23:25 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+// Current temperature.
+const kelvin = 0;
+// Converting Calvin to Celcius.
+const celcius = kelvin -273;
+// Convert Celcius to Fahrenhit;
+let fahrenheit = celcius * (9/5) + 32;
+// Rounddown to fahrenheit variable.
+Fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature ${Fahrenheit} degrees Fahrenheit.`);
